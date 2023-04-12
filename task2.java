0f77abe8d44dd992dedf9331865144efd2e544f2
kavipreetha74 <127287566+kavipreetha74@users.noreply.github.com>
2023-04-12 18:03:52 +0530

    Add files via upload

---
+++ task2.java
@@ -0,0 +1,43 @@
+package cia1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class task2 {
+public static void main(String[] args)
+{
+WebDriverManager.chromedriver().setup();
+WebDriver driver;
+ChromeOptions co = new ChromeOptions();
+co.addArguments("--remote-allow-origins=*");
+driver = new ChromeDriver(co);
+
+driver.navigate().to("https://www.amazon.in/Apple-MacBook-Laptop-12%E2%80%91core-38%E2%80%91core/dp/B0BSHVCXN9/ref=sr_1_1?crid=2JOW8ZEP2G4PW&keywords=macbook+air+m2+pro+max&qid=1681292234&sprefix=macbook+air+m2+pro%2Caps%2C578&sr=8-1");
+WebElement addtocart = driver.findElement(By.id("add-to-cart-button"));
+addtocart.click();
+WebElement check = driver.findElement(By.xpath("//*[@class=\"a-button-input\"]"));
+check.click();
+
+driver.navigate().to("https://www.amazon.in/gp/cart/view.html/ref=dp_atch_dss_cart?");
+WebElement qty = driver.findElement(By.id("a-autoid-0-announce"));
+qty.click();
+WebElement dty = driver.findElement(By.id("quantity_10"));
+dty.click();
+
+WebElement update = driver.findElement(By.id("a-autoid-1-announce"));
+update.click();
+
+driver.navigate().to("https://www.amazon.in/gp/cart/view.html/ref=dp_atch_dss_cart?");
+WebElement del = driver.findElement(By.xpath("//*[@class=\"a-color-link\"]"));
+del.click();
+
+
+}
+}
+
+
